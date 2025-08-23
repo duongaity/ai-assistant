65537d5a2646ec263a31a13e5b31e5aef9990d9f
Duong Aity <duongaity@gmail.com>
2025-08-23 14:00:23 +0700

    update

--- frontend/src/services/apiService.js
+++ frontend/src/services/apiService.js
@@ -9,6 +9,7 @@
  */
 
 const API_BASE_URL = 'http://ec2-44-204-83-69.compute-1.amazonaws.com:8888/api';
+// const API_BASE_URL = 'http://localhost:8888/api';
 class ApiService {
   constructor() {
     this.sessionId = this.getOrCreateSessionId();
