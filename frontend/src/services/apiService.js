f65992a79da1dc08b017f6dbd0d0f9c3e824f5e9
Duong Aity <duongaity@gmail.com>
2025-08-23 13:14:43 +0700

    update

--- frontend/src/services/apiService.js
+++ frontend/src/services/apiService.js
@@ -8,7 +8,7 @@
  * - Cache management
  */
 
-const API_BASE_URL = 'http://backend:8888/api';
+const API_BASE_URL = 'http://us-east-1.console.aws.amazon.com/';
 class ApiService {
   constructor() {
     this.sessionId = this.getOrCreateSessionId();
